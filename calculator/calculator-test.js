a3f2c633e650dd7ecf672d261c7d5631a0ee9d4d
Brian Giusti <brianjohngiusti@gmail.com>
2022-09-27 07:18:07 -0400

    more

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,31 @@
 
 it('should calculate the monthly rate correctly', function () {
+  expect(calculateMonthlyPayment(100,10,5)).toEqual('200.00');
+});
+
+//from solution
+it('should calculate the monthly rate correctly', function() {
   // ...
+  const values = {
+    amount: 10000,
+    years: 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('130.44');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  expect(calculateMonthlyPayment(100,10,5)).toEqual('200.00');
 });
 
-/// etc
+
+//from solution
+it("should return a result with 2 decimal places", function() {
+  const values = {
+    amount: 10043,
+    years: 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('131.00');
+});
