86e1a6a08fe8d751e9c742fed21c67bc2d92a7e8
Brian Giusti <brianjohngiusti@gmail.com>
2022-10-06 15:16:40 -0400

    finished

---
+++ index.js
@@ -0,0 +1,25 @@
+var PI = 3.14;
+
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+let pi = 3.14
+pi = 42
+
+const Pi = 3.14
+
+
+//Quiz
+/*
+What is the difference between var and let?
+var is scoped in the whole file, let is block scoped. Var can be redeclared let cannot
+
+What is the difference between var and const?
+var is not block scoped like const. 
+
+What is the difference between let and const?
+const cannot be reassigned or redeclared 
+
+What is hoisting?
+When code is compiled and the declaration of the variable happens first
+*/
