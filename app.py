2a0ce49ac4646979a4cb80bd59f1bf99cbedb368
Brian Giusti <brianjohngiusti@gmail.com>
2022-12-30 13:43:01 -0500

    done

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def fill_form():
+    prompts = story.prompts
+    return render_template("form.html", prompts=prompts)
+
+
+@app.route("/story")
+def create_story():
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
